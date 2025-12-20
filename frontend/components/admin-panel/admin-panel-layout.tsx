3f442e95af20cda4a62e78fe5114db97cf343233
parin <dagonparinya@gmail.com>
2025-12-20 23:14:28 +0700

    Remove unused router import and simplify main background styling in AdminPanelLayout

--- frontend/components/admin-panel/admin-panel-layout.tsx
+++ frontend/components/admin-panel/admin-panel-layout.tsx
@@ -5,7 +5,6 @@ import { Sidebar } from "@/components/admin-panel/sidebar";
 import { useSidebar } from "@/hooks/use-sidebar";
 import { useStore } from "@/hooks/use-store";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
 
 export default function AdminPanelLayout({ children }: { children: React.ReactNode }) {
   const sidebar = useStore(useSidebar, (x) => x);
@@ -17,7 +16,7 @@ export default function AdminPanelLayout({ children }: { children: React.ReactNo
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
+          "min-h-[calc(100vh-56px)] transition-[margin-left] ease-in-out duration-300",
           !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72")
         )}>
         {children}
